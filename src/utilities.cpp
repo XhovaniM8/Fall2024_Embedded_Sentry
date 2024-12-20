1711df5149b4d80772770b0f9dacff81eab606b0
Xhovani Mali <xhovani03@live.com>
2024-12-19 23:43:27 -0500

    Update utilities.cpp

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -5,13 +5,11 @@
  * @version 0.1
  * @date 2024-12-15
  *
- * @copyright Copyright (c) 2024
  *
  * @group Members:
- * - Xhovani Mali (xxm202)
- * - Shruti Pangare (stp8232)
- * - Temira Koenig (trk8600)
- * - Shruti Tulshidas Pangare
+ * - Xhovani Mali 
+ * - Shruti Pangare 
+ * - Temira Koenig 
  */
 
 #include <array>
