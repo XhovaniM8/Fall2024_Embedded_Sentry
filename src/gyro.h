ba2840a50b3cb5339281699af81f40c992f644bc
Xhovani Mali <xhovani03@live.com>
2024-12-19 23:12:36 -0500

    Update gyro.h

--- src/gyro.h
+++ src/gyro.h
@@ -1,7 +1,8 @@
 /**
  * @file gyro.h
  * @author Xhovani Mali (xxm202)
- * @brief Header file for gyroscope functionality in the embedded sentry project.
+ * @brief Header file for gyroscope functionality in the embedded sentry
+ * project.
  * @version 0.1
  * @date 2024-12-15
  *
@@ -14,32 +15,29 @@
  * - Shruti Tulshidas Pangare
  */
 
-
 #include <mbed.h>
+
 #include "system_config.h"
 
 // Initialization parameters
-typedef struct
-{
-    uint8_t conf1;       // output data rate
-    uint8_t conf3;       // interrupt configuration
-    uint8_t conf4;       // full sacle selection
+typedef struct {
+  uint8_t conf1;  // output data rate
+  uint8_t conf3;  // interrupt configuration
+  uint8_t conf4;  // full sacle selection
 } Gyroscope_Init_Parameters;
 
 // Raw data
-typedef struct
-{
-    int16_t x_raw; // X-axis raw data
-    int16_t y_raw; // Y-axis raw data
-    int16_t z_raw; // Z-axis raw data
+typedef struct {
+  int16_t x_raw;  // X-axis raw data
+  int16_t y_raw;  // Y-axis raw data
+  int16_t z_raw;  // Z-axis raw data
 } Gyroscope_RawData;
 
 // Calibrated data
-typedef struct
-{
-    int16_t x_calibrated; // X-axis calibrated data
-    int16_t y_calibrated; // Y-axis calibrated data
-    int16_t z_calibrated; // Z-axis calibrated data
+typedef struct {
+  int16_t x_calibrated;  // X-axis calibrated data
+  int16_t y_calibrated;  // Y-axis calibrated data
+  int16_t z_calibrated;  // Z-axis calibrated data
 } Gyroscope_CalibratedData;
 
 // Write IO
@@ -52,7 +50,8 @@ void GetGyroValue(Gyroscope_RawData *rawdata);
 void CalibrateGyroscope(Gyroscope_RawData *rawdata);
 
 // Gyroscope initialization
-void InitiateGyroscope(Gyroscope_Init_Parameters *init_parameters, Gyroscope_RawData *init_raw_data);
+void InitiateGyroscope(Gyroscope_Init_Parameters *init_parameters,
+                       Gyroscope_RawData *init_raw_data);
 
 // Data conversion: raw -> dps
 float ConvertToDPS(int16_t rawdata);
